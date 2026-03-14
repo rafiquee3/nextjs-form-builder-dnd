c787e4fa1e9057c350d65e5d0e49b24ce62c8f66
Rafal <rafiquee.code@gmail.com>
2026-03-14 09:54:15 +0100

    feat: implement dynamic import for FormBuilderArea with SSR disabled

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 'use client';
-import FormBuilderArea from "@/src/components/FormBuilderArea";
+//import FormBuilderArea from "@/src/components/FormBuilderArea";
 import PalettePanel from "@/src/components/PalettePanel";
 import PalettePanelMobile from "@/src/components/PalettePanelMobile";
 import PropertiesPanel from "@/src/components/PropertiesPanel";
 import { useEffect, useState } from "react";
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import dynamic from 'next/dynamic';
+
+const FormBuilderArea = dynamic(
+  () => import('@/src/components/FormBuilderArea'), // ścieżka do Twojego komponentu
+  { ssr: false }
+);
 
 export default function BuilderPage() {
   const [isMounted, setIsMounted] = useState(false);
